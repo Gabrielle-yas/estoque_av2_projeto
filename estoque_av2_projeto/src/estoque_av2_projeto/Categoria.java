1ea86f77244910bbf25b8c454465e2d93bf2599a
Gabrielle Oliveira <gabrielleoliveira.yasmin@gmail.com>
2025-05-24 15:09:26 -0300

    segundo commit

---
+++ estoque_av2_projeto/src/estoque_av2_projeto/Categoria.java
@@ -0,0 +1,5 @@
+package estoque_av2_projeto;
+
+public enum Categoria {
+    MERCEARIA, HIGIENE_PESSOAL, PRODUTO_DE_LIMPEZA, FLV, PETSHOP, BEBIDA, ACOUGUE
+}
---
+++ estoque_av2_projeto/src/estoque_av2_projeto/Estoque.java
@@ -0,0 +1,9 @@
+package estoque_av2_projeto;
+
+import java.util.List;
+
+public class Estoque {
+    private Produto produto;
+    private Usuario usuario;
+
+}
